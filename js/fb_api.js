c7baca7ebd4cb2e512841487e11508ed2ea3d924
Cynmil <43480842+Cynmil@users.noreply.github.com>
2019-11-06 16:59:19 -0800

    added new features

--- js/fb_api.js
+++ js/fb_api.js
@@ -1,12 +1,25 @@
 // page id: 211483252619589
-// access token: none
+// access token: EAAhhpQ4YeRIBAOQUZAwS5jAhl7vNMYija4s0gybsvZAlpyjZCOh9iuuzYpjkqmlZAk6SWbOsVDycKNMDZCR0xtvhZBd493TFGY29TKM2KuX2ZBtaprrS9ZCtao6187EeVnawCPXQli9MKI3xmSYHvW2PNvvvSbZCIfDuxPCDzGax1WiwJMkrfQWiU
+
+// FB event image (I want to see when it expires) (created in 10/23 5:42pm)
+// https://scontent.xx.fbcdn.net/v/t1.0-9/s720x720/72436160_778070729294169_557818330791018496_o.jpg?_nc_cat=104&_nc_oc=AQkCI7V5aeptXPevZOaNUAfFtGD-yPV3gv0RVA-DEdwQCxkrF7Ghk0ccbh-6FHFdiGQ&_nc_ht=scontent.xx&oh=4fb231e5caf8540404e43a3c61590388&oe=5E62AB92
+
+
 
 function testFunction(callback) {
   const path = '';
   const method = '';
   const params = {};
 
-  FB.api(path, method, params, callback);
+  FB.api(
+    '/211483252619589/events?fields=id,start_date,name,cover',
+    'GET',
+    {access_token: "EAAhhpQ4YeRIBAOQUZAwS5jAhl7vNMYija4s0gybsvZAlpyjZCOh9iuuzYpjkqmlZAk6SWbOsVDycKNMDZCR0xtvhZBd493TFGY29TKM2KuX2ZBtaprrS9ZCtao6187EeVnawCPXQli9MKI3xmSYHvW2PNvvvSbZCIfDuxPCDzGax1WiwJMkrfQWiU"
+    },
+    function(response) {
+        console.log(response);
+    }
+  );
 }
 
 
@@ -16,19 +29,21 @@ function displayEvents(events) {
 
 
 function fetchPageEvents(callback) {
-  FB.ui({
-    method: 'share',
-    href: 'https://developers.facebook.com/docs/'
-  }, callback);
+  // FB.ui({
+  //   method: 'share',
+  //   href: 'https://developers.facebook.com/docs/'
+  // }, callback);
+  FB.api();
 }
 
 
-// window.fbAsyncInit = function() {
-//   FB.init({
-//     appId            : '2359161347668242',
-//     autoLogAppEvents : true,
-//     xfbml            : true,
-//     version          : 'v4.0'
-//   });
-//   testFunction(() => console.log('hi'));
-// };
+window.fbAsyncInit = function() {
+  FB.init({
+    appId            : '2359161347668242',
+    autoLogAppEvents : true,
+    xfbml            : true,
+    version          : 'v4.0'
+  });
+  testFunction(() => console.log('hi'));
+};
+
---
+++ nodejs/app.js
@@ -0,0 +1,3 @@
+console.log('hello world from Node.js')
+
+let fb = require('fb')
